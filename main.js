acb510aad9615ccd252f201bd3c84a1c6b0bf317
Pano0013 <capoparodi@hotmail.com>
2021-11-28 14:49:22 -0300

    .

---
+++ main.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("tarea");
+const boton = document.getElementsByClassName("btn");
+const tabla = document.getElementById("tabla"); 
+
+function nuevaTarea(){
+
+    const tarea = document.createElement("tr");
+
+    tarea.innerHTML = `
+     <td> <input type="checkbox" class="checkbox" onClick="completar()" /> </td>
+     <td > ${input.value} </td>
+     <td>  X  </td>
+    `;
+console.log(input.value);
+    tabla.appendChild(tarea);
+};
+
+boton.addEventListener("click", nuevaTarea);
